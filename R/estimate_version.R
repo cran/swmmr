30bcfa74512c5f45172999b69b2373d5eda613d5
Dominik Leutnant <leutnant@fh-muenster.de>
2019-03-18 20:50:05 +0000

    version 0.9.0

---
+++ R/estimate_version.R
@@ -0,0 +1,17 @@
+#' import helper
+#' @keywords internal
+estimate_version <- function(x, ...) {
+  UseMethod("estimate_version", x)
+}
+
+#' import helper
+#' @keywords internal
+estimate_version.inp <- function(x, ...){
+  
+}
+
+#' import helper
+#' @keywords internal
+estimate_version.rpt <- function(x, ...){
+  
+}
